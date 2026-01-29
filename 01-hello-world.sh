91f2006cbaf2abd43483ed5b254393c1a876a36e
Manideep-devops28 <mareddy036@gmail.com>
2026-01-29 23:17:32 +0530

    hello world completed

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
+# This is a simple shell script
 
-echo "Enter a number:"
-read num
-
-if [ $num -gt 10 ]
-then
-    echo "Number is greater than 10"
-else
-    echo "Number is less than or equal to 10"
-fi
+echo "Hello World"
